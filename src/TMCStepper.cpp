068581992eb044617ea85eda548dae0ff9baddec
Tie <twb4252@rit.edu>
2026-04-13 15:15:49 -0400

    update pins of stepper motors (y and z)

--- src/TMCStepper.cpp
+++ src/TMCStepper.cpp
@@ -1,28 +1,32 @@
-#include <TMC2130Stepper.h>
-
-// Rambo v1.1 X-Axis Pin Map
-#define X_STEP_PIN    37
-#define X_DIR_PIN     48
-#define X_ENABLE_PIN  29
-#define X_CS_PIN      41 
 #include "TMCStepper.h"
+
 
 // Constructor using the FULL hardware SPI pinout for Rambo 1.1b
 // Arguments: (CS, RSENSE, MOSI, MISO, SCK)
-TMC2130Stepper TMC2130 = TMC2130Stepper(X_ENABLE_PIN,  X_DIR_PIN, X_STEP_PIN, X_CS_PIN);
+TMC2130Stepper TMC2130X = TMC2130Stepper(X_ENABLE_PIN,  X_DIR_PIN, X_STEP_PIN, X_CS_PIN); // it is a x-motion 
+TMC2130Stepper TMC2130Y = TMC2130Stepper(Y_ENABLE_PIN,  Y_DIR_PIN, Y_STEP_PIN, Y_CS_PIN); // it is a y-motion 
+TMC2130Stepper TMC2130Z = TMC2130Stepper(Z_ENABLE_PIN,  Z_DIR_PIN, Z_STEP_PIN, Z_CS_PIN); // it is a z-motion 
 
 //global variable
 bool dir = true;
 
 void initializeStepper(){
-    TMC2130.begin(); // Initiate pins and registeries
-    TMC2130.SilentStepStick2130(600); // Set stepper current in mA
-    TMC2130.stealthChop(1); // Enable extremely quiet stepping, if 0, it is LOUD. Probably would worn down the internal gears?
-
+    TMC2130X.begin(); // Initiate pins and registeries
+    TMC2130X.SilentStepStick2130(600); // Set stepper current in mA
+    TMC2130X.stealthChop(1); // Enable extremely quiet stepping, if 0, it is LOUD. Probably would worn down the internal gears?
+    TMC2130Y.begin();
+    TMC2130Y.SilentStepStick2130(600); 
+    TMC2130Y.stealthChop(1); 
+    TMC2130Z.begin();
+    TMC2130Z.SilentStepStick2130(600); 
+    TMC2130Z.stealthChop(1); 
     digitalWrite(X_ENABLE_PIN, LOW);
+    digitalWrite(Y_ENABLE_PIN, LOW);
+    digitalWrite(Z_ENABLE_PIN, LOW);
 }
 
 void testingTMCstepper(bool enabled){
+    // this does work!
     if (enabled){
         digitalWrite(X_STEP_PIN, HIGH);
         delayMicroseconds(10); //These delay controls the speed. lower = faster.  
@@ -33,14 +37,15 @@ void testingTMCstepper(bool enabled){
         if ((ms - last_time) > 1000) {
             if (dir) {
                 Serial.println("Dir -> 0");
-                TMC2130.shaft_dir(0);
+                TMC2130X.shaft_dir(0);
             } else {
                 Serial.println("Dir -> 1");
-                TMC2130.shaft_dir(1);
+                TMC2130X.shaft_dir(1);
             }
-        dir = !dir;
-        Serial.println(TMC2130.GCONF(), BIN);
-        last_time = ms;
-    }
+            dir = !dir;
+            Serial.println(TMC2130X.GCONF(), BIN);
+            last_time = ms;
+        }
     }
 }
+
--- src/TMCStepper.h
+++ src/TMCStepper.h
@@ -2,6 +2,28 @@
 #define TESTTMCSTEPPER_H
 
 #include <Arduino.h>
+#include <TMC2130Stepper.h>
+
+// RAMBo v1.2b X-Axis Pin Map
+#define X_STEP_PIN    37 // on Atmega 2560 slot
+#define X_DIR_PIN     48 // PD5
+#define X_ENABLE_PIN  29 // NC7-GND
+#define X_MS1_PIN     64 // Microstepping 1
+#define X_CS_PIN     41 // Microstepping 2 (Formerly X_CS_PIN)
+
+// RAMBo v1.2b Y-Axis Pin Map
+#define Y_STEP_PIN    36 
+#define Y_DIR_PIN     49
+#define Y_ENABLE_PIN  28 
+#define Y_MS1_PIN     69 
+#define Y_CS_PIN     39 
+
+// RAMBo v1.2b Z-Axis Pin Map
+#define Z_STEP_PIN    35 
+#define Z_DIR_PIN     47
+#define Z_ENABLE_PIN  27 
+#define Z_MS1_PIN     68 
+#define Z_CS_PIN      67
 
 void initializeStepper();
 void testingTMCstepper(bool enabled);
